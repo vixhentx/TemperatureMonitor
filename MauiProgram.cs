28be14647ecfa25c39ad553ba5b8db8290b947ad
淼涛 温 <w1084349470@outlook.com>
2025-04-26 11:33:34 +0800

    import iconfont

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,7 @@ namespace TemperatureMonitor
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("iconfont.ttf", "iconfont");
                 });
 
 #if DEBUG
